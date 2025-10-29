ea9626b5d838c39ffe6547b71fab917ee2b9f020
Devaki1316 <kandalamdevaki1316@gmail.com>
2025-10-29 11:06:00 +0530

    Update deployment setup for Render

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+set -o errexit  # Exit on error
+
+# Upgrade pip to the latest
 pip install --upgrade pip
-pip install -r requirements.txt
+
+# Install all packages from requirements.txt (only prebuilt wheels)
+pip install --only-binary=:all: -r requirements.txt
